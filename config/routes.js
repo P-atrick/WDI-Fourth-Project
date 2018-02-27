fd0db79d7050ed4e16511af437258acb23b93d50
Patrick Kelly <patrick@pkelly.co>
2018-02-27 14:39:48 +0000

    added routes to config

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
+const meteorites = require('../controllers/meteorites');
+const auth   = require('../controllers/auth');
+const secureRoute = require('../lib/secureRoute');
 
-// routes go here
+router.route('/meteorites')
+  .get(meteorites.index)
+  .post(secureRoute, meteorites.create);
+
+router.route('/meteorites/:id')
+  .get(meteorites.show)
+  .put(secureRoute, meteorites.update)
+  .delete(secureRoute, meteorites.delete);
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
 
 router.all('/*', (req, res) => res.notFound());
 
