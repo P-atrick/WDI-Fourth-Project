af8ceed806e77b84dbef6ae884bd344ef5fe8df1
Patrick Kelly <patrick@pkelly.co>
2018-02-27 15:40:44 +0000

    added MeteoritesLanding

--- src/components/meteorites/MeteoritesRoutes.js
+++ src/components/meteorites/MeteoritesRoutes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import ProtectedRoute from '../utility/ProtectedRoute';
 
+import MeteoritesLanding from './MeteoritesLanding';
 import MeteoritesIndex from './MeteoritesIndex';
 import MeteoritesNew from './MeteoritesNew';
 
 const MeteoritesRoutes = () => {
   return(
     <Switch>
+      <Route exact path="/" component={ MeteoritesLanding } />
       <Route exact path="/meteorites" component={ MeteoritesIndex } />
       <ProtectedRoute path="/meteorites/new" component={MeteoritesNew} />
     </Switch>
