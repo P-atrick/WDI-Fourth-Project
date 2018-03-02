0732d9b735955dc79da5f89d32ba1d6e5a0f9b09
Patrick Kelly <patrick@pkelly.co>
2018-03-02 13:53:32 +0000

    added dropdown to sort meteorites on MeteoritesIndex

--- src/components/meteorites/MeteoritesIndex.js
+++ src/components/meteorites/MeteoritesIndex.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
+
+import SearchBar from './SearchBar';
 
 class MeteoritesIndex extends Component {
 
   state = {
-    meteorites: []
+    meteorites: [],
+    sortBy: '',
+    sortDirection: ''
   }
 
   componentWillMount() {
@@ -15,19 +20,37 @@ class MeteoritesIndex extends Component {
       .catch(err => console.log(err));
   }
 
+  handleSort = e => {
+    const [sortBy, sortDirection] = e.target.value.split('|');
+    this.setState({ sortBy, sortDirection });
+  }
+
+  sortFilter() {
+    const { sortBy, sortDirection } = this.state;
+
+    const orderedMeteorites = _.orderBy(this.state.meteorites, [sortBy], [sortDirection]);
+    return orderedMeteorites;
+  }
+
   render() {
+    const meteorites = this.sortFilter();
     return(
-      <div className="columns is-multiline is-mobile">
-        {this.state.meteorites.map(meteorite => {
-          return(
-            <div key={meteorite.id} className="column is-one-quarter">
-              <Link to={`/meteorites/${meteorite.id}`}>
-                <img src={meteorite.image} className="image-square"/>
-              </Link>
-              <h3 className="meteorite-info">{meteorite.name} - {meteorite.weight}g</h3>
-            </div>
-          );
-        })}
+      <div>
+        <SearchBar
+          handleSort={ this.handleSort }
+        />
+        <div className="columns is-multiline is-mobile">
+          {meteorites.map(meteorite => {
+            return(
+              <div key={meteorite.id} className="column is-one-quarter">
+                <Link to={`/meteorites/${meteorite.id}`}>
+                  <img src={meteorite.image} className="image-square"/>
+                </Link>
+                <h3 className="meteorite-info">{meteorite.name} - {meteorite.weight}g</h3>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
---
+++ src/components/meteorites/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SearchBar = ({ handleSort }) => {
+
+  return(
+    <div>
+      <select onChange={handleSort}>
+        <option value="name|asc">Name (A - Z)</option>
+        <option value="name|desc">Name (Z - A)</option>
+        <option value="weight|asc">Weight (Low - High)</option>
+        <option value="weight|desc">Weight (High - Low)</option>
+        <option value="height|asc">Height (Low - High)</option>
+        <option value="height|desc">Height (High - Low)</option>
+        <option value="length|asc">Length (Low - High)</option>
+        <option value="length|desc">Length (High - Low)</option>
+        <option value="width|asc">Width (Low - High)</option>
+        <option value="width|desc">Width (High - Low)</option>
+      </select>
+    </div>
+  );
+};
+
+export default SearchBar;
